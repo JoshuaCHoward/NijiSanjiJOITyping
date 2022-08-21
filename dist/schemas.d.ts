7f41074bff84ace7875a7dcd6859bd6d0372db80
JoshuaCHoward <JoshuaCHoward@users.noreply.github.com>
2022-08-21 22:08:00 +0000

    Updated distribution files

---
+++ dist/schemas.d.ts
@@ -0,0 +1,4 @@
+import Joi from 'joi';
+export declare const ChangedPairingSchema: Joi.ObjectSchema<any>;
+export declare const YourVoiceTranscriptTheirVoiceSchema: Joi.ObjectSchema<any>;
+//# sourceMappingURL=schemas.d.ts.map
