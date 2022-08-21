bf9715ffef3ae9eb342e7bcc5c3e43881b35f455
HowardCJ <joshua.howard@meetkai.com>
2022-08-21 18:35:04 -0400

    Changed typings main

--- dist/schemas.d.ts
+++
@@ -1,4 +0,0 @@
-import Joi from '@hapi/joi';
-export declare const ChangedPairingSchema: Joi.ObjectSchema;
-export declare const YourVoiceTranscriptTheirVoiceSchema: Joi.ObjectSchema;
-//# sourceMappingURL=schemas.d.ts.map
